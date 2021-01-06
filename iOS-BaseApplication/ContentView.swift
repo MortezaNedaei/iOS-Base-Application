1c8a77c9cbda96827f9d5c50b7719ef83bc45583
Morteza Nedaei <nedaei1375@gmail.com>
2021-01-06 16:09:49 +0330

    Initial Commit

---
+++ iOS-BaseApplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-BaseApplication
+//
+//  Created by MoNe on 10/17/1399 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
